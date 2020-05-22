ed418553b6a8966509f2d46e3cf98feaf81d009a
Zaryab Shahzaib <zaryabshahzaib1@gmail.com>
2020-05-22 21:19:24 +0300

    done with user model and user admin

--- users/models.py
+++ users/models.py
@@ -5,4 +5,35 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    """ Custom User Model """
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_PKR = "pkr"
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_PKR, "PKR"))
+
+    bio = models.TextField(default="", blank=True)
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    birthdate = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True
+    )
+    currency = models.CharField(
+        max_length=3, choices=CURRENCY_CHOICES, null=True, blank=True
+    )
+    superhost = models.BooleanField(default=False)
